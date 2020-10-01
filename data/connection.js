2f35c1d30fa0bd9f1b90d27a240f1947cfa42a01
Gaston Iglesias <giglesias@sml.com.ar>
2020-10-01 16:26:01 -0300

    up

--- data/connection.js
+++ data/connection.js
@@ -41,10 +41,10 @@ async function runQuery(queryString) {
     var resultadoJson;
 
     var db = mysql.createConnection({
-        host: 'localhost', //10.78.160.5
+        host:  '10.78.160.5', //'localhost', //10.78.160.5
         database: 'turnosd',
-        user: 'root', //turnos
-        password: 'password' // '142857' //'V8Rf4ZfbpS'
+        user: 'turnos', //turnos
+        password: 'V8Rf4ZfbpS' // password // '142857' //'V8Rf4ZfbpS'
       });
 
     /*var db = mysql.createConnection({
